44e6da6aac835870eb6692c9b9a4e128ee06baf5
mumbuamutuku <mumbuamutuku@gmail.com>
2023-03-27 04:25:36 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#create a file in tmp called school
+file { '/tmp/school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+}
